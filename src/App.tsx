c6e016819b10a3dfa07f3a903a681bec736db666
Jamshad10 <jamshadtsy143@gmail.com>
2023-07-26 16:16:46 +0530

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { useEffect, type FC } from 'react';
 import './App.css';
 import Header from './component/Header';
 import Home from 'pages/Home';
@@ -10,8 +10,19 @@ import Store from 'Redux/Store';
 import { Routes, Route } from 'react-router-dom'
 import UserList from 'pages/UserView';
 import UpdateUser from 'pages/UpdateUser';
+import axios from 'axios';
 
 const App: FC = () => {
+
+  const fetchData = async () => {
+    const results = await axios.get('http://localhost:8000/user');
+    console.log(results);
+    
+  };
+
+  useEffect(() => {
+    fetchData()
+  },[])
 
   return (
     <Provider store={Store}>
